987f37b6581b5c3dac460371923adc3cfb2957c8
Simon Date <simon@adaptfreelancer.com>
2021-01-29 12:33:24 +0100

    First setup

---
+++ src/components/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class Tabs extends React.Component {
+
+    render() {
+        return `<div> cool </div>`
+    }
+}
---
+++ src/entries/entry.js
@@ -0,0 +1,8 @@
+var React = require('React');
+var ReactDOM = require('React-dom');
+var App = require('./scripts/app');
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+)
