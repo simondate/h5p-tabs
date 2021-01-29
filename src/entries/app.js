ea773e1595f4cf49b130bd0e7726352530eb632f
Simon Date <simon@adaptfreelancer.com>
2021-01-29 13:29:59 +0100

    Copied structure from other project

---
+++ src/entries/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tabs from '../styles/components/tabs';
+
+ReactDOM.render(
+    <Tabs />,
+    document.getElementById('app')
+)
--- src/entries/entry.js
+++
@@ -1,8 +0,0 @@
-var React = require('React');
-var ReactDOM = require('React-dom');
-var App = require('./scripts/app');
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-)
